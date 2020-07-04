1e735dfb874585960fb835edabdf32506961c516
tedisy47123 <tedisya47@gmail.com>
2020-07-05 02:11:43 +0700

    tedi

--- image_description/views.py
+++ image_description/views.py
@@ -17,14 +17,14 @@ from PIL import Image
 import os
 from pickle import dump, load
 import numpy as np
-# from keras.applications.xception import Xception, preprocess_input
-# from keras.preprocessing.image import load_img, img_to_array
-# from keras.preprocessing.text import Tokenizer
-# from keras.preprocessing.sequence import pad_sequences
-# from keras.utils import to_categorical
-# from keras.layers.merge import add
-# from keras.models import Model, load_model
-# from keras.layers import Input, Dense, LSTM, Embedding, Dropout
+from keras.applications.xception import Xception, preprocess_input
+from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers.merge import add
+from keras.models import Model, load_model
+from keras.layers import Input, Dense, LSTM, Embedding, Dropout
 # small library for seeing the progress of loops.
 from tqdm import tqdm_notebook as tqdm
 # tqdm().pandas()
